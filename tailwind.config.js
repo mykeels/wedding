94351e9f56324966f221a7950cd61e46058c39a5
mykeels <mykehell123@gmail.com>
2021-02-07 09:49:31 +0100

    chore: extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: theme => ({
+        "128": "30rem",
+        "196": "36rem",
+      }),
+      screens: {
+        '3xl': { min: '1700px' }
+      }
+    },
   },
   variants: {
     extend: {},
+  },
+  fontFamily: {
+    sans: ["Vibes", "Alex", "Scriptina", "Helvetica", "Arial"]
   },
   plugins: [],
 };
