8799a027ffaef1e5e57e254913ea87bc20c0def4
mykeels <mykehell123@gmail.com>
2021-02-07 07:57:16 +0100

    chore: add build scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
